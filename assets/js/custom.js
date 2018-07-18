6351ce3d36811342a20e6bdff28617115ed753c0
Prakash <prakasharul94@gmail.com>
2018-07-18 17:54:07 +0530

    wordslider + animate

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip();
-});
+
 
 $(document).ready(function() {
+	$('[data-toggle="tooltip"]').tooltip();
+
     $('.carousel').carousel();
+
+
+    $("#wordslider").wordsrotator({
+					stopOnHover: true,
+					words: ['Reliability ','Security','Quality','Satisfaction','Support'],
+					animationIn: "rotateInDownLeft",
+					animationOut: "rotateOutDownLeft"
+				});
 });
